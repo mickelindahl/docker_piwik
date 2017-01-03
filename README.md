cb149f1c2752b1183bbdb7d0b63a9ce7680e3ba0
Loredfe [C[C[D[D[3~[3~ge AB <apps@loredge.com>
2017-01-03 18:18:29 +0000

    up and running on subdomain TODO:  ssl and description

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# docker_piwik
+# Docker piwik
+
+
---
+++ config.ini.php
@@ -0,0 +1,5 @@
+[General]
+proxy_client_headers[] = "HTTP_X_FORWARDED_FOR"
+#proxy_client_headers[] = "X-Real-IP"
+proxy_host_headers[] = "HTTP_X_FORWARDED_HOST"
+#proxy_ips[] = ""
